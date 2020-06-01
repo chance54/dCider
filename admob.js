76a8080a387e1dd9a9231b1d9e264307f08bbbc2
chance54 <53322470+chance54@users.noreply.github.com>
2020-06-01 18:13:37 -0400

    Update admob.js

--- admob.js
+++ admob.js
@@ -1,11 +1,9 @@
-function onLoad() {
-    alert("loaded");
-    // Set AdMobAds options:
-    admob.setOptions({
-        publisherId:          "ca-app-pub-3940256099942544/6300978111",  // Required
-        interstitialAdId:     "	ca-app-pub-3940256099942544/1033173712",  // Optional
-    });
-}
+alert("loaded");
+// Set AdMobAds options:
+admob.setOptions({
+publisherId:          "ca-app-pub-3940256099942544/6300978111",  // Required
+interstitialAdId:     "ca-app-pub-3940256099942544/1033173712",  // Optional
+});
 
 
 admob.createBannerView({
@@ -15,3 +13,4 @@ admob.createBannerView({
 admob.requestInterstitialAd({
     autoShowInterstitial: true
 });
+alert("admobend");
